f63e5b995e0ce7b4c9f90ca4c6af6339b7ebed0f
yoshihiko <horrormovies.noga@gmail.com>
2018-10-08 20:41:27 +0900

    refactor: ロガーの使用と未使用変数削除

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,16 +1,13 @@
 import Vue from 'vue';
 
-export default function (context) {
-  const {
-    store,
-    redirect,
-  } = context;
-  console.log('middleware/check-auth');
+export default function(context) {
+  const { store } = context;
+  Vue.$log.debug('middleware/check-auth');
   // ユーザーが認証されていないとき
   if (!store.getters['Auth/checkAuth']) {
-    console.log('NOTTO OUSUDO');
+    Vue.$log.debug('NOTTO OUSUDO');
     // return redirect('/login')
   } else {
-    console.log('OUSUDO');
+    Vue.$log.debug('OUSUDO');
   }
 }
