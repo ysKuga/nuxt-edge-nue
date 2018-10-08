9f80e6d7591472450a00d18b38feab6427916476
yoshihiko <horrormovies.noga@gmail.com>
2018-10-08 20:09:28 +0900

    feat: prettier かんれん
    - [参考](https://github.com/prettier/eslint-plugin-prettier)

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,13 +14,19 @@ module.exports = {
     'plugin:vue/recommended',
     // semi colon のため vue 関連の後に
     'eslint:recommended',
+    "plugin:prettier/recommended",
   ],
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'vue',
+    "prettier",
   ],
   // add your custom rules here
   rules: {
+    "prettier/prettier": ["error", {
+      // https://github.com/prettier/prettier/issues/2280#issuecomment-311326803
+      "singleQuote": true
+    }],
     "comma-dangle": [2, "always-multiline"],
   }
 };
