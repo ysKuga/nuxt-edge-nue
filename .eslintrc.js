f0841a51d2b87da52ce1cc7d3e77951ac37c7982
yoshihiko <horrormovies.noga@gmail.com>
2018-10-08 20:10:37 +0900

    style: いちおうシングルクォートに

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,30 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    "es6": true,
+    'es6': true,
   },
   parserOptions: {
     parser: 'babel-eslint',
-    "ecmaVersion": 2018,
+    'ecmaVersion': 2018,
   },
   extends: [
     'vue',
     'plugin:vue/recommended',
     // semi colon のため vue 関連の後に
     'eslint:recommended',
-    "plugin:prettier/recommended",
+    'plugin:prettier/recommended',
   ],
   // required to lint *.vue files
   plugins: [
     'vue',
-    "prettier",
+    'prettier',
   ],
   // add your custom rules here
   rules: {
-    "prettier/prettier": ["error", {
+    'prettier/prettier': ['error', {
       // https://github.com/prettier/prettier/issues/2280#issuecomment-311326803
-      "singleQuote": true
+      'singleQuote': true
     }],
-    "comma-dangle": [2, "always-multiline"],
+    'comma-dangle': [2, 'always-multiline'],
   }
 };
