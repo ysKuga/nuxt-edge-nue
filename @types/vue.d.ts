be7f2b0806612f51788a7d56a42845d6b382555e
yoshihiko <horrormovies.noga@gmail.com>
2018-11-07 23:45:35 +0900

    feat: layout を許容させる

--- @types/vue.d.ts
+++ @types/vue.d.ts
@@ -4,5 +4,7 @@ declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     // This adds the `middleware` property to the existing `vue/types/options/ComponentOptions` type
     middleware?: string | string[];
+    // Nuxt.js の layout 指定向け
+    layout?: string;
   }
 }
