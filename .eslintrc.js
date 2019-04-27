e0a84a765ab924871136b4165f311b13b2f3002e
yoshihiko <horrormovies.noga@gmail.com>
2019-04-27 23:30:21 +0900

    chore: 以下参考事例 2 点による
    - [Vue.jsでtypescript-eslint + Prettierを導入する](https://qiita.com/markey/items/cfeb2a07e11d43676505)
    - [How to integrate ESLint with Vue.js and Vetur in Visual Studio Code](https://alligator.io/vuejs/eslint-vue-vetur/)
      - 特にこちらは `<input />` のスペースが何故か 2 つになってしまう事例の対処となった
        `<input  />` となり、`vue/no-multi-spaces` にひっかかっていた

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,33 +3,30 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    'es6': true,
+    es6: true,
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint',
-    'ecmaVersion': 2018,
+    parser: '@typescript-eslint/parser',
   },
   extends: [
-    'vue',
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/recommended',
-    // semi colon のため vue 関連の後に
-    // 下記エラーが出たのでいったんコメントアウト
-    // Cannot read config file: c:\Users\yoshihiko\Desktop\bench\vue\nuxt-edge-nue\node_modules\eslint\conf\eslint-recommended.js
-    // 'eslint:recommended',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier',
-  ],
+  // plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {
-    'prettier/prettier': ['error', {
-      // https://github.com/prettier/prettier/issues/2280#issuecomment-311326803
-      'singleQuote': true,
-      'trailingComma': true,
-    }],
+    'prettier/prettier': [
+      'error',
+      {
+        // https://github.com/prettier/prettier/issues/2280#issuecomment-311326803
+        singleQuote: true,
+        trailingComma: true,
+      },
+    ],
     'comma-dangle': [2, 'always-multiline'],
-  }
+  },
 };
