4cb0c329fae527b5a03f6f0be2b7ef092ca3d165
yoshihiko <horrormovies.noga@gmail.com>
2019-04-28 17:04:54 +0900

    chore: HTML タグの 1 attr 1 line を prettier でできないので妥協案など
    - [issuecomment-438781791 - GitHub](https://github.com/prettier/eslint-plugin-prettier/issues/94#issuecomment-438781791)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,28 +5,45 @@ module.exports = {
     node: true,
     es6: true,
   },
-  parser: 'vue-eslint-parser',
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
   },
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    "plugin:prettier/recommended",
+    "plugin:vue/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "eslint:recommended",
   ],
   // required to lint *.vue files
-  // plugins: ['vue', 'prettier'],
+  // plugins: ["vue", "prettier"],
   // add your custom rules here
   rules: {
-    'prettier/prettier': [
-      'error',
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/indent.md#options
+    // - 上記より下記 2 つの指定がインデントを 2 にする
+    indent: "off",
+    "@typescript-eslint/indent": ["error", 2],
+    // https://github.com/prettier/eslint-plugin-prettier/issues/94#issuecomment-438781791
+    // - 上記により HTML タグが 1 行あたり 80 文字を超過した場合に属性が 1 つ 1 行となる・・・
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    "prettier/prettier": [
+      "error",
       {
         // https://github.com/prettier/prettier/issues/2280#issuecomment-311326803
         singleQuote: true,
         trailingComma: true,
       },
     ],
-    'comma-dangle': [2, 'always-multiline'],
+    "comma-dangle": [2, "always-multiline"],
   },
 };
