2afe5c085a48f34106a516782f975c5388742802
yoshihiko <horrormovies.noga@gmail.com>
2019-05-20 15:35:43 +0900

    fix: eslint-disable-next-line への指定に no-unused-vars がなんかきかなくなってた

--- plugins/toast.js
+++ plugins/toast.js
@@ -1,5 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-// import { Middleware } from '@nuxt/vue-app';
+// eslint-disable-next-line
+import { Middleware } from '@nuxt/vue-app';
 
 /** @type Middleware  */
 const middleware = function() {};
