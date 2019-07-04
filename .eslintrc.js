3ec656cba0761bbcc2d2812014f476dab50a111c
yoshihiko <horrormovies.noga@gmail.com>
2019-07-04 20:41:46 +0900

    feat: eslint での `vue/html-self-closing` を prettier のために無効化
    - [ESLint/Prettier併用で<img />が怒られる問題](https://qiita.com/yoshinbo/items/3436face1a1b02f2542f)

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,16 @@ module.exports = {
   // plugins: ["vue", "prettier"],
   // add your custom rules here
   rules: {
+    // https://qiita.com/yoshinbo/items/3436face1a1b02f2542f
+    // 上記の内容をコピー
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+        },
+      },
+    ],
     // https://github.com/prettier/eslint-plugin-prettier/issues/94#issuecomment-438781791
     // - 上記により HTML タグが 1 行あたり 80 文字を超過した場合に属性が 1 つ 1 行となる・・・
     'vue/max-attributes-per-line': [
