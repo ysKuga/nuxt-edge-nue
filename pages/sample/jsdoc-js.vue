50aa28e0623a822f2dbbf772e1ff5e01d836621b
yoshihiko <horrormovies.noga@gmail.com>
2019-08-13 21:55:29 +0900

    feat: JSDoc で型指定

---
+++ pages/sample/jsdoc-js.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  /**
+   * @typedef {Object<string, any>} Props
+   * @property {string|number} propId ぷろっぷあいでい
+   */
+  /** @type {import("vue/types/options").RecordPropsDefinition<Props>} */
+  props: {
+    propId: {
+      type: String,
+      default: () => 'aidei',
+    },
+  },
+  /**
+   * @typedef {Object<string, any>} Data
+   * @property {number} id あいでぃい
+   * @property {string} name なまえ
+   */
+  /** @type {import("vue/types/options").DataDef<Data, Props, Vue>} */
+  data() {
+    return {};
+  },
+  /**
+   * @typedef {Object<string, any>} Computed
+   * @property {string} computedId けいさんされたあいでい
+   */
+  /** @type {import("vue/types/options").Accessors<Computed>} */
+  computed: {},
+  mounted() {},
+  /**
+   * @typedef {Object<string, Function>} Methods
+   * @property {{(name): boolean}} checkName なまえをちぇっくするめそっど
+   */
+  /** @type {Methods} */
+  methods: {},
+});
+</script>
