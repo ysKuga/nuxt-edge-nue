5c425423abc6f24da3068eccf9e49574b78a9c3a
yoshihiko <horrormovies.noga@gmail.com>
2019-09-21 19:48:26 +0900

    chore: Vuex のヘルパーなぱたーん使用例

---
+++ pages/sample/infer-js-blank.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    infer js blank {{ $store.getters['profile/getNamae'] }} {{ currentUser }}
+  </div>
+</template>
+
+<script>
+import { profileComputed, profileMethods } from '@/store/profile/helper';
+
+export default {
+  computed: {
+    ...profileComputed,
+  },
+  mounted() {
+    this;
+  },
+  methods: {
+    ...profileMethods,
+  },
+};
+</script>
