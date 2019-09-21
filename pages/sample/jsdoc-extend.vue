684328ad713c229bff7760fb7ecf1b14ca40ca5b
yoshihiko <horrormovies.noga@gmail.com>
2019-09-21 19:48:46 +0900

    chore: JSDoc による注釈のぱたーん

---
+++ pages/sample/jsdoc-extend.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+// // @ts-check
+import Vue from 'vue';
+
+export default Vue.extend({
+  /**
+   * @typedef {Object<string, any>} Props
+   * @property {string|number} propId ぷろっぷあいでい
+   */
+  /** @type {import("vue/types/options").RecordPropsDefinition<Props>} */
+  props: {
+    propId: {
+      type: String,
+      default: () => 'aidei',
+    },
+  },
+  /**
+   * @typedef {Object<string, any>} Data
+   * @property {number} id あいでぃい
+   * @property {string} name なまえ
+   */
+  /** @typedef {import("vue/types/options").DataDef<Data, Props, Vue>} DataDef */
+  /**
+   * @type {DataDef}
+   * @return {Data}
+   */
+  data() {
+    return {};
+  },
+  /**
+   * @typedef {Object<string, any>} Computed
+   * @property {string} computedId けいさんされたあいでい
+   */
+  /** @type {import("vue/types/options").Accessors<Computed>} */
+  computed: {},
+  mounted() {},
+  /**
+   * @typedef {Object<string, Function>} Methods
+   * @property {{(name): boolean}} checkName なまえをちぇっくするめそっど
+   */
+  /** @type {Methods} */
+  methods: {},
+});
+</script>
---
+++ pages/sample/jsdoc.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+// // @ts-check
+import Vue from 'vue';
+import {
+  // eslint-disable-next-line
+  ThisTypedComponentOptionsWithRecordProps as Options
+} from 'vue/types/options';
+
+/**
+ * @typedef {Object<string, any>} Props
+ * @property {string|number} propId ぷろっぷあいでい
+ */
+/**
+ * @typedef {Object<string, any>} Data
+ * @property {number} id あいでぃい
+ * @property {string} name なまえ
+ */
+/**
+ * @typedef {Object<string, any>} Computed
+ * @property {string} computedId けいさんされたあいでい
+ */
+/**
+ * @typedef {Object<string, Function>} Methods
+ * @property {{(name): boolean}} checkName なまえをちぇっくするめそっど
+ */
+
+/** @type {Options<Vue, Data, Methods, Computed, Props>} */
+export const options = {
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+
+export default Vue.extend(options);
+</script>
