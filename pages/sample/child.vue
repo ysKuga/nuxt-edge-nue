bf98135d8477c73d08de19a0f03fcc33f35f84c0
yoshihiko <horrormovies.noga@gmail.com>
2019-09-21 19:47:19 +0900

    chore: `vs(scp)` なかんじでつかうやつ

---
+++ pages/sample/child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    child
+    <slot :id="id" :namae="namae" />
+  </div>
+</template>
+
+<script>
+/**
+ * @typedef {Object<string, any>} SlotProps
+ * @property {number} id
+ * @property {string} namae
+ */
+/**
+ * @param {SlotProps} slotProps
+ * @return {SlotProps}
+ */
+export const vs = slotProps => slotProps;
+
+export default {
+  data() {
+    return {
+      id: 1,
+      namae: 'Taro',
+    };
+  },
+};
+</script>
---
+++ pages/sample/parent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    parent
+    <child>
+      <template v-slot="slotProps">
+        child 使用
+        {{ vs(slotProps) }}
+        {{ vs(slotProps).id }}
+        {{ vs(slotProps).namae }}
+        <!-- {{ vs(slotProps).abababab }} -->
+      </template>
+    </child>
+  </div>
+</template>
+
+<script>
+import Child, { vs } from './child.vue';
+
+export default {
+  components: {
+    Child,
+  },
+  methods: {
+    vs,
+  },
+};
+</script>
